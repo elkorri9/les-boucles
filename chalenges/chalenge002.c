38008e4aef49e15e2a0c3c7b7b5437003fbd787a
otmane <elkorriotmane@gmail.com>
2024-09-26 12:04:11 +0200

    elkorri

---
+++ chalenges/chalenge002.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a , i , rs =1;
+    int b ;
+   
+   printf("saisir un nombre : ");
+   scanf("%d",&a);
+   
+   
+   for(i=1 ; i<=a ; i++){
+       rs = i * rs ;
+       
+   }
+   printf("la factorielle d'un nombre que vous saisir : %d =%d",a,rs);
+
+    return 0;
+}
